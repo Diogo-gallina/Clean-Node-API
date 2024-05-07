b40e8e6ee194b55eeefadada76d1e48ae55de66c
diogo_gallina <diogogallina20@gmail.com>
2024-05-07 11:33:21 -0300

    refactor: change descriptio test "test" to "it"

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -16,20 +16,20 @@ const makeSut = (): EmailValidatorAdapter => {
 }
 
 describe('EmailValidator Adapter', () => {
-  test('Should return false if validator returns false', () => {
+  it('Should return false if validator returns false', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid-email@mail.com')
     expect(isValid).toBeFalsy()
   })
 
-  test('Should return true if validator returns true', () => {
+  it('Should return true if validator returns true', () => {
     const sut = makeSut()
     const isValid = sut.isValid('valid-email@mail.com')
     expect(isValid).toBeTruthy()
   })
 
-  test('Should call validator with correct email', () => {
+  it('Should call validator with correct email', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('valid-email@mail.com')
