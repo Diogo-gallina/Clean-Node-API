b539d06e486e3a0dd5994004afe4e6ff8d7bb1d7
diogo_gallina <diogogallina20@gmail.com>
2024-05-07 14:53:18 -0300

    refactor: move protocols path to data path

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,7 +3,7 @@ import {
   AddAccount,
   AddAccountModel,
 } from '../../../domain/usecases/add-account'
-import { Encrypter } from './protocols/encrypter'
+import { Encrypter } from '../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
