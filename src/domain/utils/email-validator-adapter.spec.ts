3d0e3d930ec340213ec321d7b799c9f008cba3c2
diogo_gallina <diogogallina20@gmail.com>
2024-05-07 10:06:00 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/domain/utils/email-validator-adapter.spec.ts
+++ src/domain/utils/email-validator-adapter.spec.ts
@@ -1,24 +1,36 @@
-import isEmail from "validator/lib/isEmail"
-import { EmailValidatorAdapter } from "./email-validator"
+import { EmailValidatorAdapter } from './email-validator'
 import validator from 'validator'
 
 jest.mock('validator', () => ({
-    isEmail(): boolean {
-        return true
-    }
+  isEmail(): boolean {
+    return true
+  },
 }))
 
-describe('EmailValidator Adapter', () => {
-    test('Should return false if validator returns false', () => {
-        const sut = new EmailValidatorAdapter()
-        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-        const isValid = sut.isValid('invalid-email@mail.com')
-        expect(isValid).toBeFalsy()
-    })
+beforeEach(() => {
+    jest.clearAllMocks()
+})
 
-    test('Should return true if validator returns true', () => {
-        const sut = new EmailValidatorAdapter()
-        const isValid = sut.isValid('valid-email@mail.com')
-        expect(isValid).toBeTruthy()
-    })
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid-email@mail.com')
+    expect(isValid).toBeFalsy()
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid-email@mail.com')
+    expect(isValid).toBeTruthy()
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest
+      .spyOn(validator, 'isEmail')
+    sut.isValid('valid-email@mail.com')
+    expect(isEmailSpy).toHaveBeenCalledTimes(1)
+    expect(isEmailSpy).toHaveBeenCalledWith('valid-email@mail.com')
+  })
 })
