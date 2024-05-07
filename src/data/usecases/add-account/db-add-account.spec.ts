2a4e7a2c1b7a5e959ad6af9a41e987fcaeefd8fc
diogo_gallina <diogogallina20@gmail.com>
2024-05-07 14:25:58 -0300

    refactor: ove sut creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,22 +1,36 @@
-import { DbAddAccount } from "./db-add-account"
+import { DbAddAccount } from './db-add-account'
+import { Encrypter } from './protocols/encrypter'
+
+interface SutTypes {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub {
+    async encrypt(value: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed password'))
+    }
+  }
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub,
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
-    it('Should call Encripter with correct password', async () => {
-        class EncrypterStub {
-            async encrypt(value: string): Promise<string> {
-                return new Promise(resolve => resolve('hashed password'))
-            }
-        }
-        const encrypterStub = new EncrypterStub()
-        const sut = new DbAddAccount(encrypterStub)
-        const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
-        const accountData = {
-            name: 'valid_name',
-            email: 'valid_email@mail.com',
-            password: 'valid_password',
-        }
-        await sut.add(accountData)
-        expect(encryptSpy).toHaveBeenCalledTimes(1)
-        expect(encryptSpy).toHaveBeenCalledWith('valid_password')
-    })
+  it('Should call Encripter with correct password', async () => {
+    const { sut, encrypterStub } = makeSut()
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password',
+    }
+    await sut.add(accountData)
+    expect(encryptSpy).toHaveBeenCalledTimes(1)
+    expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+  })
 })
