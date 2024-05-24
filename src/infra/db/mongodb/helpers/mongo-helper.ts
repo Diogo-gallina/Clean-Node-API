9f3073262e3eab7075acd4270a8e00f766a62cba
diogo_gallina <diogogallina20@gmail.com>
2024-05-24 19:37:00 -0300

    refactor: remove unused condition and resolve eslint error

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
 
   async connect(uri: string): Promise<void> {
-    if (this.client) await this.client.close()
-    this.client = await MongoClient.connect(process.env.MONGO_URL)
+    this.client = await MongoClient.connect(uri)
   },
 
   async disconnect(): Promise<void> {
