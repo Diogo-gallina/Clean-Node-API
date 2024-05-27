d0eff93c5c25e33415aba88bcd8f7f27607a67a3
diogo_gallina <diogogallina20@gmail.com>
2024-05-27 17:11:55 -0300

    feat: remove unused conditions

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -8,15 +8,15 @@ export const MongoHelper = {
   },
 
   async disconnect(): Promise<void> {
-    if (this.client) await this.client.close()
+    await this.client.close()
   },
 
   async getCollection(name: string): Promise<Collection> {
-    if (this.client) return this.client.db().collection(name)
+    return this.client.db().collection(name)
   },
 
   map: (collection: any): any => {
     const { _id, ...collectionWithoutId } = collection
-    return Object.assign({}, collectionWithoutId, { id: _id.toHexString()})
-  }
+    return Object.assign({}, collectionWithoutId, { id: _id.toHexString() })
+  },
 }
