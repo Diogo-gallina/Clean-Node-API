e89190cc427f9fa793655a5232095b86d4c2c675
diogo_gallina <diogogallina20@gmail.com>
2024-05-27 22:06:05 -0300

    fix: convert mongo url env type

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,18 @@
+import { MongoHelper as sut } from './mongo-helper'
+
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL as string)
+  })
+
+  beforeAll(async () => {
+    await sut.disconnect()
+  })
+  it('Should reconnect if mongo is down', async () => {
+    let accountCollection = sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
