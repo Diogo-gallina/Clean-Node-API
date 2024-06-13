c63d1f77eada677a668bf2b3405a7490f8bc952e
diogo_gallina <diogogallina20@gmail.com>
2024-06-13 16:05:17 -0300

    refactor: add makeFakeServerError factory in log test

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -25,6 +25,12 @@ const makeFakeRequest = (): HttpRequest => ({
   },
 })
 
+const makeFakeServerError = (): HttpResponse => {
+  const fakeError = new Error()
+  fakeError.stack = 'any_stack'
+  return serverError(fakeError)
+}
+
 interface SutTypes {
   sut: LogControllerDecorator
   controllerStub: Controller
@@ -74,13 +80,10 @@ describe('LogController Decorator', () => {
 
   it('Should call LogErrorRepository with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
-    const fakeError = new Error()
-    fakeError.stack = 'any_stack'
-    const error = serverError(fakeError)
     const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
     jest
       .spyOn(controllerStub, 'handle')
-      .mockReturnValueOnce(new Promise(resolve => resolve(error)))
+      .mockReturnValueOnce(new Promise(resolve => resolve(makeFakeServerError())))
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(logSpy).toHaveBeenCalledWith('any_stack')
