824ffd4d0dcd3760fb041c82d5f5742c1ed6361b
diogo_gallina <diogogallina20@gmail.com>
2024-06-24 11:13:19 -0300

    refactor: remove unused promises on login and simplify condictions on code

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -17,22 +17,12 @@ export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { email, password } = httpRequest.body
-      if (!email)
-        return new Promise(resolve =>
-          resolve(badRequest(new MissingParamError('email'))),
-        )
+      if (!email) return badRequest(new MissingParamError('email'))
 
-      if (!password)
-        return new Promise(resolve =>
-          resolve(badRequest(new MissingParamError('password'))),
-        )
+      if (!password) return badRequest(new MissingParamError('password'))
 
       const isValid = this.emailValidator.isValid(email)
-
-      if (!isValid)
-        return new Promise(resolve =>
-          resolve(badRequest(new InvalidParamError('email'))),
-        )
+      if (!isValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
       return serverError(error as Error)
     }
